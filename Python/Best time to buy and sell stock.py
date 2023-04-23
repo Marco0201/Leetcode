09c3ae2d07d03a668647c426f934885de15c412a
Marco0201 <marcoperz0201@gmail.com>
2023-04-22 19:18:21 -0700

    changed variable

--- Python/Best time to buy and sell stock.py
+++ Python/Best time to buy and sell stock.py
@@ -30,14 +30,14 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         l = 0
         r = 1
-        max_profit = 0
+        max_prof = 0
 
         while r < len(prices):
             if prices[l] < prices[r]:
                 profit = prices[r] - prices[l]
-                max_profit = max(max_profit, profit)
+                max_prof = max(max_prof, profit)
             else:
                 l = r
             r += 1
         
-        return max_profit
+        return max_prof
