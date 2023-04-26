d81fc48ef6dd14b41bc5faaa83c7319dcb1e4bdb
Marco0201 <marcoperz0201@gmail.com>
2023-04-25 20:27:42 -0700

    added leetcode solution

--- Python/Valid Parantheses.py
+++ Python/Valid Parantheses.py
@@ -27,3 +27,19 @@
 
 #     1 <= s.length <= 104
 #     s consists of parentheses only '()[]{}'.
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closeToOpen = {")": "(", "]": "[", "}": "{"}
+
+        for i in s:
+            if i in closeToOpen:
+                if stack and stack[-1] == closeToOpen[i]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(i)
+
+        return True if not stack else False
